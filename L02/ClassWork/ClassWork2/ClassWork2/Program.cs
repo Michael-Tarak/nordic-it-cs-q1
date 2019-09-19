2bef09ef980e23df0b5a4610292ff9295d6cb6b5
Michael-Tarak <tarakanovma98@gmail.com>
2019-09-20 00:25:41 +0300

    Lession_2_start

---
+++ L02/ClassWork/ClassWork2/ClassWork2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
