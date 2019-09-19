5dc93300a2fd1c900f05ba84ccce46823290ef11
Michael-Tarak <tarakanovma98@gmail.com>
2019-09-19 18:29:20 +0300

    Update1

---
+++ L01/HomeWork/Homework1/Homework1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter name");
+        string name = Console.ReadLine();
+        Thread.Sleep(5000);
+        Console.WriteLine($"Hello {name}!");
+        Thread.Sleep(5000);
+        Console.WriteLine($"See you soon, {name}");
+        Console.ReadKey();
+    }
+}
