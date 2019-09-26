c6005f20ae321929ba115df8f7ebf389d984c389
lemur1998 <lemur1998@mail.ru>
2019-09-27 01:17:59 +0300

    CW4_Colors

--- L04/ClassWork4/ClassWork4/Program.cs
+++ L04/ClassWork4/ClassWork4/Program.cs
@@ -2,22 +2,36 @@
 
 namespace ClassWork4
 {
-    enum Season : int
-    {
-        Зима = 3,
-        Весна = 6,
-        Лето = 9,
-        Осень = 12
-    };
+    //enum Season : int
+    //{
+    //    Зима = 3,
+    //    Весна = 6,
+    //    Лето = 9,
+    //    Осень = 12
+    //};
+    [Flags] enum Colors:short {Черный  = 1, Синий = 2, Циан = 4, Серый = 8, Зеленый = 16, Пурпурный = 32, Красный = 64, Белый = 128, Желтый = 256};
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Введите номер сезона");
-            int seasonNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"{(Season)seasonNumber} находится под номером {seasonNumber}");
+            Console.WriteLine("Список цветов:");
+            foreach(short i in Enum.GetValues(typeof(Colors)))
+            {
+                Console.WriteLine($"{(Colors)i} - {i}");
+            }
+            Colors favoriteColors = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"Выберите {i+1}-й цвет: ");
+                favoriteColors = favoriteColors | (Colors)Int16.Parse(Console.ReadLine());
+            }
+            Colors notFavoriteColors = (Colors)(~(short)favoriteColors^65024);
+            Console.WriteLine($"Избранные вами цвета: {favoriteColors}\nОставшиеся цвета: {notFavoriteColors}");
+            //Console.WriteLine(Convert.ToString((short)favoriteColors, toBase: 2));
+            //Console.WriteLine(Convert.ToString((short)notFavoriteColors,toBase: 2));
         }
            
     }
