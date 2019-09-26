d820846fe3417bd190e137a9a6272f405004dbed
lemur1998 <lemur1998@mail.ru>
2019-09-26 21:20:46 +0300

    Cw4_season

--- L04/ClassWork4/ClassWork4/Program.cs
+++ L04/ClassWork4/ClassWork4/Program.cs
@@ -2,29 +2,23 @@
 
 namespace ClassWork4
 {
+    enum Season : int
+    {
+        Winter = 3,
+        Spring = 6,
+        Summer = 9,
+        Autumn = 12
+    };
     class Program
     {
         static void Main(string[] args)
         {
             Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            double a;
-            double h;
-            Console.WriteLine("Введите значение стороны основания");
-            while (true)
-            {
-                if (double.TryParse(Console.ReadLine(), out a))
-                    break;
-            }
-            Console.WriteLine("Введите значение высоты");
-            while(true)
-            {
-                if (double.TryParse(Console.ReadLine(), out h))
-                    break;
-            }
-            Console.WriteLine($"Площадь боковой стороны: {3.0 * a * h}");
-            Console.WriteLine($"Полная площадь фигуры: {(3.0/2.0)*a*(a*Math.Cbrt(3.0)+2.0*h)}");
-            Console.WriteLine($"Объем фигуры: {(Math.Pow(a,2)/2.0)*h*Math.Cbrt(3.0)}");
+            Console.WriteLine("Введите номер сезона");
+            int seasonNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{(Season)seasonNumber}");
         }
+           
     }
 }
