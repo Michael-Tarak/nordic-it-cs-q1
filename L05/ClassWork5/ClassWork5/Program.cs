7a1b2c090571718f7c2b019d06ab40b9ef444fd4
lemur1998 <lemur1998@mail.ru>
2019-10-01 19:42:46 +0300

    cl5_equal100

---
+++ L05/ClassWork5/ClassWork5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double output;
+            while(true)
+            {
+                if(double.TryParse(Console.ReadLine(), out output))
+                {
+                    break;
+                }
+            }
+            if (output > 100)
+            {
+                Console.WriteLine("Input is more than 100");
+            }
+            else
+            {
+                Console.WriteLine("Input is less or equal 100");
+            }
+        }
+    }
+}
