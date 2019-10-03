4a1fdc8b88d097d2903419b736b4871021ecb3c3
lemur1998 <lemur1998@mail.ru>
2019-10-03 19:09:16 +0300

    cl5_exit

---
+++ L06/ClassWork6/ClassWork6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassWork6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            do
+            {
+                Console.WriteLine("exit to complete program");
+                 input = Console.ReadLine();
+            }
+            while (input != "exit");
+        }
+    }
+}
