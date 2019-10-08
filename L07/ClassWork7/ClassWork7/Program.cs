7d55ef49841f6006005784f943d05b0b21053eae
lemur1998 <lemur1998@mail.ru>
2019-10-08 19:42:12 +0300

    cl7strings

---
+++ L07/ClassWork7/ClassWork7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassWork7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введите первый символ");
+            double firstOperand = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второй символ");
+            double secondOperand = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{firstOperand:#.##} * {secondOperand:#.##} = {firstOperand * secondOperand:#.##}");
+            Console.WriteLine($"{firstOperand:#.##} + {secondOperand:#.##} = {firstOperand + secondOperand:#.##}");
+            Console.WriteLine($"{firstOperand:#.##} - {secondOperand:#.##} = {firstOperand - secondOperand:#.##}");
+
+
+
+        }
+    }
+}
