36e7de689263b5f7c75b2aaa6396ce06351d94e9
lemur1998 <lemur1998@mail.ru>
2019-10-10 20:09:49 +0300

    cl8_mid

---
+++ L08/ClassWork8/ClassWork8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassWork8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            string input;
+            var firstList = new List<double>();
+            try
+            {
+                double sum = 0;
+                double result = 0;
+                while (true)
+                {
+                    Console.WriteLine("Введите число, для остановки введите stop");
+                    input = Console.ReadLine();
+                    if (input == "stop")
+                    {
+                        for (int i = 0; i < firstList.Count; i++)
+                        {
+                            sum += firstList[i];
+                        }
+                        result = sum / (double)firstList.Count;
+                        break;
+
+                    }
+                    firstList.Add(double.Parse(input));
+                }
+                Console.WriteLine($" Ответ: {result}");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка ввода!");
+            }
+            
+        }
+    }
+}
