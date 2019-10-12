b39015c66feb26bd6a6a085ed8cd42c78171ab55
lemur1998 <lemur1998@mail.ru>
2019-10-12 18:57:20 +0300

    cl8_firstre

--- L08/ClassWork8/ClassWork8/Program.cs
+++ L08/ClassWork8/ClassWork8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ClassWork8
 {
@@ -9,20 +10,61 @@ namespace ClassWork8
         {
             Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
+            while(true)
+            {
+                Console.Write("ВЫберите номер самостоятельной работы: ");
+                var input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                switch (input)
+                {
+                    case "1":
+                        FirstClassWork();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine("Неправильный номер работы");
+                        break;
+                }
+            }
+        }
+
+        private static void FirstClassWork()
+        {
             var countriesAndCapitals = new Dictionary<string, string>
             {
                 ["Россия"] = "Москва",
                 ["США"] = "Вашингтон",
                 ["Великобритания"] = "Лондон",
                 ["Румыния"] = "Бухарест",
-                ["Болгария"] = "София"
+                ["Болгария"] = "София",
+                ["Венгрия"] = "Будапешт",
+                ["Чехия"] = "Прага",
+                ["Германия"] = "Берлин"
             };
-            bool answer = true;
-            while(answer)
+            var rightCounter = 0;
+            while (true)
             {
-                var random = new Random();
-                var question = random.Next(0, countriesAndCapitals.Count);
-                Console.WriteLine($"Напишите столицу {countriesAndCapitals[question]}");
+                var random = (new Random()).Next(countriesAndCapitals.Count);
+                KeyValuePair<string, string> cac = countriesAndCapitals.ElementAtOrDefault(random);
+                var country = cac.Key;
+                var capital = cac.Value;
+                Console.Write($"Напишите название столицы страны \"{country}\": ");
+                var answer = Console.ReadLine();
+                answer.Trim();
+                if (answer != capital)
+                {
+                    Console.WriteLine($"Ответ неверный, вы проиграли. Вы отгдали {rightCounter} столиц");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ответ правильный!");
+                    rightCounter++;
+                }
             }
         }
     }
