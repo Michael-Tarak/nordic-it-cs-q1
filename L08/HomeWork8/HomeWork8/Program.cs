dbae936236d5e42a48038e6ba4f5ee36cc196203
lemur1998 <lemur1998@mail.ru>
2019-10-14 19:50:43 +0300

    hw8_f

---
+++ L08/HomeWork8/HomeWork8/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork8
+{
+    class Program
+    {
+        static Stack<bool> squareBrakets = new Stack<bool> { };
+        static Stack<bool> circleBrakets = new Stack<bool> { };
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            while(true)
+            {
+                Console.WriteLine("Введите строку, используя символы \"[\" , \"]\" , \"(\", \")\"");
+                var mainInput = Console.ReadLine();
+                if (mainInput.ToLower() == "exit")
+                    break;
+                Console.WriteLine(Qualifier(mainInput));
+                squareBrakets.Clear();
+                circleBrakets.Clear();
+            }
+        }
+
+        private static bool Qualifier(string input)
+        {
+            foreach (var symbol in input)
+            {
+                switch (symbol)
+                {
+                    case '(':
+                        circleBrakets.Push(true);
+                            break;
+                    case ')':
+                        if(circleBrakets.Count == 0)
+                            return false;
+                        circleBrakets.Pop();
+                        break;
+                    case '[':
+                        squareBrakets.Push(true);
+                        break;
+                    case ']':
+                        if (squareBrakets.Count == 0)
+                            return false;
+                        squareBrakets.Pop();
+                        break;
+                    default:
+                        Console.WriteLine("Присутсвует лишний символ!");
+                        return false;
+                }
+            }
+            if (squareBrakets.Count == 0 && circleBrakets.Count == 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
