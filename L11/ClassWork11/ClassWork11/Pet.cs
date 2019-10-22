24392e0ba1761a9407aadb2d3b463c445b885cda
lemur1998 <lemur1998@mail.ru>
2019-10-22 19:28:10 +0300

    clwk11one

---
+++ L11/ClassWork11/ClassWork11/Pet.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassWork11
+{
+    class Pet
+    {
+        public string Kind;
+        public string Name;
+        public char Sex;
+        public byte Age;
+        public string Description
+        {
+            get { return $"{Name} is a {Kind} ({Sex}) of {Age} years old"; }
+        }
+    }
+}
---
+++ L11/ClassWork11/ClassWork11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassWork11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pet1 = new Pet()
+            {
+                Name = "Alex",
+                Kind = "Lion",
+                Sex = 'M',
+                Age = 20
+            };
+            Console.WriteLine(pet1.Description);
+            var pet2 = new Pet()
+            {
+                Name = "Morty",
+                Kind = "Zebra",
+                Sex = 'M',
+                Age = 19
+            };
+            Console.WriteLine(pet2.Description);
+            Console.ReadKey();
+        }
+    }
+}
