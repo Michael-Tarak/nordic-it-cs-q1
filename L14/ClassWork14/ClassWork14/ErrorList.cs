036a0c62b618ec80afd96178fdc3868e8d412bbb
lemur1998 <lemur1998@mail.ru>
2019-10-31 19:42:55 +0300

    clwk14first

---
+++ L14/ClassWork14/ClassWork14/ErrorList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassWork14
+{
+    class ErrorList : IDisposable
+    {
+        public string Category { get; }
+        public List  <string> ErrorListCount;
+        public ErrorList(string category, List<string> list)
+        {
+            Category = category;
+            ErrorListCount = list;
+        }
+        public void Dispose()
+        {
+            ErrorListCount.Clear();
+            ErrorListCount = null;
+        }
+    }
+}
---
+++ L14/ClassWork14/ClassWork14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassWork14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listArray = new List<string>();
+            using var someErrorList = new ErrorList("Problems",listArray);
+            someErrorList.ErrorListCount.Add("problem1");
+            someErrorList.ErrorListCount.Add("problem2");
+            for(int i = 0; i < someErrorList.ErrorListCount.Count; i++)
+            {
+                Console.WriteLine($"{someErrorList.Category}: {someErrorList.ErrorListCount[i]}");
+            }
+        }
+    }
+}
