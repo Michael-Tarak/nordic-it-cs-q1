3b801a444233c84fd149afc14d816d196d533a0c
Michael-Tarak <tarakanovma98@gmail.com>
2019-11-05 20:21:19 +0300

    clwk15first

--- L13/HomeWork13/HomeWork13/Classes/MultipleLogWriter.cs
+++ L13/HomeWork13/HomeWork13/Classes/MultipleLogWriter.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-
-namespace HomeWork13
+﻿namespace HomeWork13
 {
     class MultipleLogWriter :AbstractLogWriter, ILogWriter
     {
--- L15/ClassWork15/ClassWork15/Program.cs
+++ L15/ClassWork15/ClassWork15/Program.cs
@@ -2,11 +2,31 @@
 
 namespace ClassWork15
 {
+    class Account<T>
+    {
+        public T Id { get; private set; }
+        public string Name { get; private set; }
+        public Account ( T id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void WriteProperties()
+        {
+            Console.WriteLine($"{nameof(Id)}: {Id}\n" +
+                $"{nameof(Name)}: {Name}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var accountInteger = new Account<int>(1, "John");
+            var accountString = new Account<string>("2", "John");
+            var accountGuid = new Account<Guid>(Guid.NewGuid(), "John");
+            accountInteger.WriteProperties();
+            accountString.WriteProperties();
+            accountGuid.WriteProperties();
         }
     }
 }
