1a24cca7b88f132e1f836b4b529eb75dbb830dd4
Michael-Tarak <Tarakanovma98@gmail.com>
2019-11-07 21:50:54 +0300

    clwk16trouble

---
+++ L16/ClassWork16/Calculator.Operations/CircleCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Calculator.Figures;
+
+namespace Calculator.Operations
+{
+    class CircleCalculation
+    {
+        public CircleCalculation(Circle circle)
+        {
+            Console.WriteLine(circle.Calculate(PerimeterCalculator));
+            Console.WriteLine(circle.Calculate(SquareCalculator));
+        }
+        public static double PerimeterCalculator(double radius)
+        {
+            return 2 * Math.PI * radius;
+        }
+        public static double SquareCalculator(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+}
--- L16/ClassWork16/ClassWork16/Program.cs
+++ L16/ClassWork16/ClassWork16/Program.cs
@@ -1,28 +1,15 @@
 ﻿using System;
+using Calculator.Figures;
+using Calculator.Operations;
 
 namespace ClassWork16
 {
-    public sealed class Circle
-    {
-        private double _radius;
-        public Circle(double radius)
-        {
-            _radius = radius;
-        }
-        public double Calculate (Func<double, double> operation)
-        {
-            return operation(_radius);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Circle circle1 = new Circle(1.5);
-            Func<double,double> square = (x) => Math.PI * x * x;
-            Func<double,double> perimeter = (x) => 2 * x * Math.PI;
-            Console.WriteLine(circle1.Calculate(square));
-            Console.WriteLine(circle1.Calculate(perimeter));
+            Circle circle1 = new Circle(10);
+            var calc = new ci
         }
         
     }
