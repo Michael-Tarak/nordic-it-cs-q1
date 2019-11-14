66c0873da89cd7c77bbe74c874b9e605a483f645
Michael-Tarak <Tarakanovma98@gmail.com>
2019-11-14 20:33:30 +0300

    clwk18first

---
+++ ChatBot/Reminder/Reminder.Storage/IReminderStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Reminder.Storage
+{
+    /// <summary>
+    /// Определяет основные методы хранилища
+    /// </summary>
+    public interface IReminderStorage
+    {
+        bool Create(
+            string message,
+            string contactId,
+            DateTimeOffset messageDate);
+        void Update(
+            string message,
+            string contactId,
+            DateTimeOffset messageDate);
+        ReminderItem FindById(Guid id);
+    }
+}
--- L17/ClassWork17/ClassWork17/Program.cs
+++ L17/ClassWork17/ClassWork17/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Threading;
 
 namespace ClassWork17
 {
+    public class RandomDateGeneratedHandler
+    {
+        public int BytesDone { get; set; }
+        public int TotalBytes { get; set; }
+    }
+    class RandomDateGenerator
+    {
+        public event EventHandler<RandomDateGeneratedHandler> RandomDataGenerating;
+        public event EventHandler RandomDateGenerated;
+        public byte [] GetRandomData(int datasize, int bytesDoneToRaiseEvent)
+        {
+            var fullData = new byte[datasize];
+            for (int i = 0; i < fullData.Length; i++)
+            {
+                Random random = new Random();
+                fullData[i] = (byte)random.Next();
+                if(i % bytesDoneToRaiseEvent == 0 && i != 0)
+                {
+                    RandomDataGenerating?.Invoke(i, fullData.Length);
+                }
+            }
+            RandomDateGenerated(this, null);
+            return fullData;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var generator = new RandomDateGenerator();
+            generator.RandomDataGenerating += Generator_RandomDataGenerating;
+            generator.RandomDateGenerated += Generator_RandomDateGenerated;
+            var data = generator.GetRandomData(1000, 150);
+
+        }
+
+        private static void Generator_RandomDateGenerated(object sender, EventArgs e)
+        {
+            Console.WriteLine("Generation done");
+        }
+
+        private static void Generator_RandomDataGenerating(int bytesDone, int totalBytes)
+        {
+            Console.WriteLine($"Generate {bytesDone} out of {totalBytes}");
         }
     }
 }
